b4fc7ec1ca6078506fa0344ec6d23b019245237c
MasudRana924 <masud15-924@diu.edu.bd>
2021-10-28 23:42:17 -0600

    admin panle

--- index.js
+++ index.js
@@ -59,6 +59,29 @@ async function run() {
             const result = await ordersCollection.insertOne(order)
             res.json(result)
         })
+        // get from orders
+        app.get('/orders',async(req,res)=>{
+            const cursor = ordersCollection.find({})
+            const orders = await cursor.toArray()
+            res.send(orders)
+        })
+         //  single orders id
+         app.get('/orders/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const user = await ordersCollection.findOne(query)
+
+            console.log(id)
+            res.send(user)
+        })
+        // delete 
+         //  delete api 
+         app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await ordersCollection.deleteOne(query)
+            res.json(result)
+        })
 
     }
     finally {
